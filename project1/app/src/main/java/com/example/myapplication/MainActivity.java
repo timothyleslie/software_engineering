89948cafe0a798b11b84fb2d98547eac3261b66a
timothyleslie <836241487@qq.com>
2020-10-06 00:08:02 +0800

    second commit

--- project1/app/src/main/java/com/example/myapplication/MainActivity.java
+++ project1/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,10 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button mBtnStatistics, mBtnSetting, mBtnAdd;
+    private Button mBtnStatistics, mBtnSetting;
+    private ImageButton mBtnAdd;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
